2b3b5d59f93e8e9fd412776654ad17cfb7fda7bb
Markus Hobisch <markus.hobisch@gmx.at>
2025-09-27 19:48:13 +0200

    Chore: add comprehensive .gitignore and drop tracked build artifacts
    - Ignore build/, bin/, object and dependency files, CMake cache
    - Remove previously committed build directory from index
    - Keep source + test harness only

--- build/CMakeFiles/SecasyAvalanche.dir/Calculations.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/SecasyAvalanche.dir/Calculations.c.obj: \
- C:\Users\marku\IdeaProjects\Secasy\Calculations.c \
- C:\Users\marku\IdeaProjects\Secasy\Calculations.h \
- C:\Users\marku\IdeaProjects\Secasy\Defines.h
